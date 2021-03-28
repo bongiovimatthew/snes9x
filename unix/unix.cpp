fc0302ef150c186cd38f6c5f4c0a420f71a78f13
Matt Bongiovi <bongiovimatthew@gmail.com>
2021-03-28 11:06:01 +0100

    Fixes memory writing logic

--- unix/unix.cpp
+++ unix/unix.cpp
@@ -1194,7 +1194,7 @@ int main (int argc, char **argv)
     portno = 3004;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
-        error("ERROR opening socket");
+        perror("ERROR opening socket");
     server = gethostbyname("localhost");
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
@@ -1207,7 +1207,7 @@ int main (int argc, char **argv)
          server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+        perror("ERROR connecting");
 
 	// Main unix loop
 	while (1)
